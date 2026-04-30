c8a78130fee23e15e43185e3c6d9527f5837a136
ln985 <3200563400@qq.com>
2026-04-30 18:23:30 +0800

    fix: update channel name

--- app/src/main/java/com/warzone/changer/App.kt
+++ app/src/main/java/com/warzone/changer/App.kt
@@ -7,7 +7,7 @@ import android.os.Build
 
 class App : Application() {
     companion object {
-        const val CHANNEL_ID = "mock_location_service"
+        const val CHANNEL_ID = "warzone_vpn_service"
         lateinit var instance: App; private set
         const val API_BASE = "https://lnzdy.xf79.cn/api"
     }
@@ -15,7 +15,7 @@ class App : Application() {
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(CHANNEL_ID, "战区修改服务", NotificationManager.IMPORTANCE_LOW).apply {
-                description = "模拟定位服务通知"
+                description = "VPN代理服务通知"
             }
             getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
         }
